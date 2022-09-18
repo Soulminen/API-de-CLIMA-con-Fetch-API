2d4ff2f5fa2ee8cc3412bc0f35910a691f45aae7
Yamil Escobar <joseyamilej@hotmail.com>
2022-09-19 00:10:54 +0200

    Mostrando un Spinner de carga Proyecto finalizado

--- js/app.js
+++ js/app.js
@@ -62,6 +62,8 @@ function consultarAPI(ciudad, pais) {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
+    Spinner();
+
     fetch(url)
         .then ( respuesta => respuesta.json())
         .then( datos => {
@@ -75,11 +77,11 @@ function consultarAPI(ciudad, pais) {
 
             // Imprime la respuesta en el HTML
 
-            mnostrarClima(datos);
+            mostrarClima(datos);
         })
 }
 
-function mnostrarClima(datos) {
+function mostrarClima(datos) {
     const { name, main: { temp, temp_max, temp_min } } = datos;
 
     const centigrados = kelvinACentigrados(temp);
@@ -119,4 +121,30 @@ function limpiarHTML() {
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
+}
+
+function Spinner() {
+
+    limpiarHTML();
+
+    
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `;
+
+    resultado.appendChild(divSpinner);
 }
