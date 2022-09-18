4a601e4b2271d3a1e2ae49daf9a062d65bd1fb43
Yamil Escobar <joseyamilej@hotmail.com>
2022-09-18 22:13:22 +0200

    Empezando con formularios JS

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
+})
+
+
+function buscarClima() {
+    e.preventDefault();
+
+
+    // Validar
+
+    // Consultar API
+
+}
+
