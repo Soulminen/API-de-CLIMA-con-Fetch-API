26780e6ec7baf9ef0a4f1954f4ab4adb58419e50
Yamil Escobar <joseyamilej@hotmail.com>
2022-09-18 22:53:54 +0200

    Mostrar la Temperatura Actual

--- js/app.js
+++ js/app.js
@@ -65,9 +65,40 @@ function consultarAPI(ciudad, pais) {
     fetch(url)
         .then ( respuesta => respuesta.json())
         .then( datos => {
-            console.log(datos)
+
+            limpiarHTML(); // Limpiar el HTML previo
+
             if(datos.cod === '404') {
                 mostrarError('Ciudad no encontrada')
+                return;
             }
+
+            // Imprime la respuesta en el HTML
+
+            mnostrarClima(datos);
         })
+}
+
+function mnostrarClima(datos) {
+    const { main: { temp, temp_max, temp_min } } = datos;
+
+    const centigrados = kelvinACentigrados(temp);
+    
+    const actual = document.createElement('p');
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('font-bold', 'text-6xl');
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center', 'text-white');
+    resultadoDiv.appendChild(actual);
+
+    resultado.appendChild(resultadoDiv);
+}
+
+const kelvinACentigrados = grados => parseInt(grados - 273.15)
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
